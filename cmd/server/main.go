5dbe74d29cc9a07feb462a4c6c8b39ea8bbff6c7
barmand <armand.brethes@ynov.com>
2025-10-14 15:12:38 +0200

    First Page Added To load | V1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,15 @@
 package server
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Bienvenue sur mon serveur Go !")
+	})
+
+	fmt.Println("Serveur démarré sur le port 8080...")
+	http.ListenAndServe(":8080", nil)
+}
